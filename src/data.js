a51a307bec4635e502e46154d3c1a806919bcb5c
oguzhanural <oguzhnural@gmail.com>
2023-03-03 12:56:42 +0300

    updated data.js

--- src/data.js
+++ src/data.js
@@ -66,4 +66,84 @@ export const cards = [
         img: "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_550,dpr_1.0/v1/attachments/generic_asset/asset/055f758c1f5b3a1ab38c047dce553860-1598561741678/book-covers-2x.png"
     }
 ];
+
+export const projects = [
+    {
+        id: 1,
+        img: "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_…30e232cbc989e2b-1617004615063/bruno_malagrino.png",
+        pp: "https://fiverr-res.cloudinary.com/t_profile_thumb,…68980.489561/AF1BF970-07CA-454B-8AF1-2F3E06838C8B",
+        cat: "Logo Design",
+        username: "bruno_malagrino",
+    },
+    {
+        id: 2,
+        img: "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_…1430e232cbc989e2b-1617004615096/passionshake.jpeg",
+        pp: "https://fiverr-res.cloudinary.com/t_profile_thumb,…22734146/74e5ab33-a5fc-40ae-9cee-a91b23e80237.jpg",
+        cat: "Product Photography",
+        username: "passionshake",
+    },
+    {
+        id: 3,
+        img: "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_…0e611430e232cbc989e2b-1617004615077/eveeelin.jpeg",
+        pp: "https://fiverr-res.cloudinary.com/t_profile_thumb,…94084918/f79ede47-da5f-440a-bf23-57ed9ef7d363.png",
+        cat: "Logo Design",
+        username: "eveeelin",
+    },
+    {
+        id: 4,
+        img: "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_…0e232cbc989e2b-1617004615063/annapietrangeli.jpeg",
+        pp: "https://fiverr-res.cloudinary.com/t_profile_thumb,…20928117/1d531e54-7607-4bdb-815f-088dbc0fb971.jpg",
+        cat: "Book Design",
+        username: "annapietrangeli",
+    },
+    {
+        id: 5,
+        img: "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_…30e232cbc989e2b-1617004615082/christophbrandl.png",
+        pp: "https://fiverr-res.cloudinary.com/t_profile_thumb,…9215580/6b4a9867-ad06-415f-b307-11177ae30fdd.jpeg",
+        cat: "Illustration",
+        username: "christophbrandl",
+    },
+    {
+        id: 6,
+        img: "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_…3ef0e611430e232cbc989e2b-1617004615069/noneyn.png",
+        pp: "https://fiverr-res.cloudinary.com/t_profile_thumb,…24545655/0aaffa8e-01e0-4dcb-b56d-674e9b9c4bf5.jpg",
+        cat: "Portraits & Caricatures",
+        username: "noneyn",
+    },
+    {
+        id: 7,
+        img: "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_…a8c8e6b6793b24-1617027896306/lamonastudio-img.png",
+        pp: "https://fiverr-res.cloudinary.com/t_profile_thumb,…55607494/5a706f4e-9f73-4ebc-97ff-9d2ef16bf28c.jpg",
+        cat: "Animated GIFs",
+        username: "lamonastudio",
+    },
+    {
+        id: 8,
+        img: "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_…611430e232cbc989e2b-1617004615106/skydesigner.png",
+        pp: "https://fiverr-res.cloudinary.com/t_profile_thumb,…70224917/2bb8af3c-4cce-42a8-a699-f11177524084.png",
+        cat: "Web & Mobile Design",
+        username: "skydesigner",
+    },
+    {
+        id: 9,
+        img: "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_…08e858cd563bedbc1f9e48-1617004762616/spickex.jpeg",
+        pp: "https://fiverr-res.cloudinary.com/t_profile_thumb,…ofile/photos/22711136/original/fiverr_profile.jpg",
+        cat: "Flyer Design",
+        username: "spickex",
+    },
+    {
+        id: 10,
+        img: "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_…611430e232cbc989e2b-1617004615068/mijalzagier.png",
+        pp: "https://fiverr-res.cloudinary.com/t_profile_thumb,…84111113/750ccab0-8a64-4c91-b9a4-d10039dbf79c.jpg",
+        cat: "Packaging Design",
+        username: "mijalzagier",
+    },
+    {
+        id: 11,
+        img: "https://fiverr-res.cloudinary.com/q_auto,f_auto,w_…430e232cbc989e2b-1617004615084/fernandobengua.png",
+        pp: "https://fiverr-res.cloudinary.com/t_profile_thumb,…10299120/db111eb4-c119-42b4-9a1d-9116601f3d22.png",
+        cat: "Social Media Design",
+        username: "fernandobengua",
+    }
+];
     
