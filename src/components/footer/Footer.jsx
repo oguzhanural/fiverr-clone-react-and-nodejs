8751f9a542c9b065e835e4b4ff11030b1f23bbd0
oguzhanural <oguzhnural@gmail.com>
2023-03-08 14:36:42 +0300

    footer completed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,106 @@ import "./Footer.scss"
 
 const Footer = () => {
   return (
-    <div className='footer'>Footer</div>
+    <div className='footer'>
+      <div className="container">
+        <hr />
+        <div className="top">
+          <div className="item">
+            <h2>Categories</h2>
+              <ul>
+                <li>Graphics & Design</li>
+                <li>Digital Marketing</li>
+                <li>Writing & Translation</li>
+                <li>Video & Animation</li>
+                <li>Music & Audio</li>
+                <li>Programming & Tech</li>
+                <li>Data</li>
+                <li>Business</li>
+                <li>Lifestyle</li>
+                <li>Photography</li>
+                <li>Sitemap</li>
+              </ul>
+          </div>
+          <div className="item">
+            <h2>About</h2>
+              <ul>
+                <li>Careers</li>
+                <li>Press & News</li>
+                <li>Partnerships</li>
+                <li>Privacy Policy</li>
+                <li>Terms of Service</li>
+                <li>Intellectual Property Claims</li>
+                <li>Investor Relations</li>
+                
+              </ul>
+          </div>
+          <div className="item">
+            <h2>Support</h2>
+              <ul>
+                <li>Help & Support</li>
+                <li>Trust & Safety</li>
+                <li>Selling on Fiverr</li>
+                <li>Buying on Fiverr</li>
+              </ul>
+          </div>
+          <div className="item">
+            <h2>Community</h2>
+              <ul>
+                <li>Graphics & Design</li>
+                <li>Digital Marketing</li>
+                <li>Writing & Translation</li>
+                <li>Video & Animation</li>
+                <li>Music & Audio</li>
+                <li>Programming & Tech</li>
+                <li>Data</li>
+                <li>Business</li>
+                <li>Lifestyle</li>
+                <li>Photography</li>
+                <li>Sitemap</li>
+              </ul>
+          </div>
+          <div className="item">
+            <h2>More From Fiverr</h2>
+              <ul>
+                <li>Graphics & Design</li>
+                <li>Digital Marketing</li>
+                <li>Writing & Translation</li>
+                <li>Video & Animation</li>
+                <li>Music & Audio</li>
+                <li>Programming & Tech</li>
+                <li>Data</li>
+                <li>Business</li>
+                <li>Lifestyle</li>
+                <li>Photography</li>
+                <li>Sitemap</li>
+              </ul>
+          </div>
+        </div>
+        <hr />
+        <div className="bottom">
+          <div className="left">
+            <h2>fiverr</h2>
+            <span>© Fiverr International Ltd. 2023</span>
+          </div>
+          <div className="right">
+            <div className="social">
+              <img src="https://cdn-icons-png.flaticon.com/512/25/25347.png" alt="" />
+              <img src="https://cdn-icons-png.flaticon.com/512/1384/1384005.png" alt="" />
+              <img src="https://cdn-icons-png.flaticon.com/512/3536/3536569.png" alt="" />
+              <img src="https://cdn-icons-png.flaticon.com/512/2175/2175205.png" alt="" />
+              <img src="https://cdn-icons-png.flaticon.com/512/1384/1384015.png" alt="" />
+            </div>
+            <div className="link">
+              <img src="./img/language.png" alt="" />
+              <span>English</span>
+              <img src="./img/coin.png" alt="" />
+            <span>USD</span>
+            </div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
