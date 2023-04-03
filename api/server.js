bc6b67aac6976ba86cb207ba4e0b8340a8869181
oguzhanural <oguzhnural@gmail.com>
2023-04-03 15:12:14 +0300

    test

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ dotenv.config();
 
 const connect = async () => {
     try {
-        await mongoose.connect(process.env.MONGO); 
+        await mongoose.connect(process.env.MONGO);
         console.log("Connected to MongoDB...");
     } catch (error) {
         handleError(error);
