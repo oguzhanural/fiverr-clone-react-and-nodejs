f4cda40c3b385fcdb2f9aff4719406afa5bca139
oguzhanural <oguzhnural@gmail.com>
2023-05-02 12:08:43 +0300

    reducer function

---
+++ client/src/reducers/gigReducers.js
@@ -0,0 +1,49 @@
+import { r } from "tar";
+
+export const INITIAL_STATE = {
+    userId: JSON.parse(localStorage.getItem("currentUser"))?._id,
+    title: "",
+    cat: "",
+    coverImg: "",
+    images: [],
+    desc: "",
+    shortTitle: "",
+    shortDesc: "",
+    deliveryTime: "",
+    revisionNumber: "",
+    features: "",
+    price: 0
+};
+
+
+export const reducer  = (state, action) => {
+    switch(action.type) {
+        
+        case "CHANGE_INPUT":
+            return {
+                ...state,
+                [action.payload.name]: action.payload.value
+            };
+        case "ADD_IMAGES":
+            return {
+                ...state,
+                coverImg: action.payload.coverImg,
+                images: action.payload.images
+            };
+        case "ADD_FEATURE":
+            return{
+                    ...state,
+                    features: [...state.feature, action.payload]
+            };
+        case "REMOVE_FEATURE":
+            return {
+                 ...state,
+                 features: state.features.filter(
+                    (feature) => feature !== action.payload // bu şartı sağlamayan tüm feature (elementleri) ları içerir.
+                    )
+            };
+        
+        default:
+            return state;
+    }
+}
